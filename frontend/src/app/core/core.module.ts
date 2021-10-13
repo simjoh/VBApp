cf48482a1a832faa18cfe8627e7fb15f40fe8d7a
Bengt Hellstrom <bengt.hellstrom@umu.se>
2021-10-13 15:53:21 +0200

    fix

---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,27 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    {provide: Window, useValue: window},
+  ],
+})
+export class CoreModule {
+
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    this.throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+
+  throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
+    if (parentModule) {
+      throw new Error(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
+    }
+  }
+
+}
