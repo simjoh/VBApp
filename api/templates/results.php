8240932687c99d692741ef563e395c85bd0b391c
Bengt Hellstrom <bengt.hellstrom@umu.se>
2021-11-05 22:24:15 +0100

    Lagt till möjlighet att visa vyer. Går att generera en resultatvy som tex kan visas på vb.se. Just nu hårdkodad men fungerar

---
+++ api/templates/results.php
@@ -0,0 +1,16 @@
+
+<table>
+
+
+	<?php foreach($sss as $key=>$value){ ?>
+    <?php foreach($value as $key=>$val){ ?>
+		<tr>
+			<td><?php echo $val; ?></td>
+		</tr>
+        <?php } ?>
+    <?php } ?>
+</table>
+
+
+Hello <?=$name?>!
+
