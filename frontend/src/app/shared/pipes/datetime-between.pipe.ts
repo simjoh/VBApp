2d66a94470119b7cb13050e960b9f199d39629cb
Bengt Hellstrom <bengt.hellstrom@umu.se>
2022-03-21 13:33:32 +0100

    Glömde lägga till fil

---
+++ frontend/src/app/shared/pipes/datetime-between.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'datetimeBetween'
+})
+export class DatetimeBetweenPipe implements PipeTransform {
+
+  transform(datestart: any, dateend: string,): unknown {
+
+    let currentDate = new Date();
+    const dateOne = new Date(datestart);
+    const dateTwo = new Date(dateend);
+    if (currentDate >= datestart && currentDate <= dateTwo){
+      return true
+    }
+
+    // if (currentDate > dateOne){
+    //   return true
+    // }
+
+    return false;
+  }
+
+}
