1e286db4275e0f0c64c2a26d4e11ce9ddde8b7dc
Bengt Hellstrom <bengt.hellstrom@umu.se>
2023-07-17 10:20:59 +0200

    Ny modul för hantering av anmälningar och betalning

---
+++ loppservice/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+
+$router->group(['prefix' => 'loppservice/api'], function () use ($router) {
+
+
+$router->get('/ping', function () use ($router) {
+    return "ssssssssssssdddddddddd";
+});
+
+
+$router->group(['prefix' => 'test'], function () use ($router) {
+
+
+$router->get('/ping', function () use ($router) {
+    return "ping in test";
+});
+
+
+
+});
+});
