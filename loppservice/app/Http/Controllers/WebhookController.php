f2657411e9b1f56fcc7f90877ad262393c6662c2
Bengt Hellstrom <bengt.hellstrom@umu.se>
2023-11-01 08:42:30 +0100

    Nu kan man använda rabattkod med 100% rabatt

--- loppservice/app/Http/Controllers/WebhookController.php
+++ loppservice/app/Http/Controllers/WebhookController.php
@@ -45,27 +45,56 @@ class WebhookController extends Controller
         switch ($event->type) {
             case 'checkout.session.completed':
                 $session = $event->data->object;
+                Log::debug("$session" . 'session.completed');
 
-                $this->create_order($session);
-                $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
-                if ($session->payment_status == 'paid') {
+                if ($this->nocostorder($session)) {
+                    $this->create_no_cost_order($session);
+
+                    $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
+                    if ($session->payment_status == 'paid') {
+                        if ($final_reg_payment) {
+                            $this->fulfill_no_cost_final_registration_order($session);
+                        } else {
+                            $this->fullfill_no_cost_order($session);
+                        }
+                    }
+                } else {
+                    $this->create_order($session);
+
+                    $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
+                    if ($session->payment_status == 'paid') {
+                        if ($final_reg_payment) {
+                            $this->fulfill_final_registration_order($session);
+                        } else {
+                            $this->fullfill_order($session);
+                        }
+                    }
+                }
+
+
+                break;
+
+            case 'checkout.session.async_payment_succeeded':
+                $session = $event->data->object;
+                Log::debug("$session" . 'async_payment_succeeded');
+
+                if ($this->nocostorder($session)) {
+                    $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
+                    if ($final_reg_payment) {
+                        $this->fulfill_no_cost_final_registration_order($session);
+                    } else {
+                        $this->fullfill_no_cost_order($session);
+                    }
+                } else {
+                    // Fulfill the purchase
+                    $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
                     if ($final_reg_payment) {
                         $this->fulfill_final_registration_order($session);
                     } else {
                         $this->fullfill_order($session);
                     }
                 }
-                break;
 
-            case 'checkout.session.async_payment_succeeded':
-                $session = $event->data->object;
-                // Fulfill the purchase
-                $final_reg_payment = filter_var($session->metadata->is_final_registration_on_event, FILTER_VALIDATE_BOOLEAN);
-                if ($final_reg_payment) {
-                    $this->fulfill_final_registration_order($session);
-                } else {
-                    $this->fullfill_order($session);
-                }
                 break;
 
             case 'checkout.session.async_payment_failed':
@@ -94,17 +123,30 @@ class WebhookController extends Controller
     private function create_order($session)
     {
         $registration = Registration::find($session->client_reference_id);
-
         if (!$registration) {
             http_response_code(404);
             exit();
         }
-
         $order = new Order();
         $order->order_id = Uuid::uuid4();
         $order->registration_uid = $registration->registration_uid;
         $order->payment_intent_id = $session->payment_intent;
         $order->payment_status = $session->payment_status;
+        $order->save();
+    }
+
+    private function create_no_cost_order(mixed $session)
+    {
+        $registration = Registration::find($session->client_reference_id);
+        if (!$registration) {
+            http_response_code(404);
+            exit();
+        }
+        $order = new Order();
+        $order->order_id = Uuid::uuid4();
+        $order->registration_uid = $registration->registration_uid;
+        $order->payment_intent_id = 'promotion_code';
+        $order->payment_status = 'paid';
         $order->save();
     }
 
@@ -116,7 +158,6 @@ class WebhookController extends Controller
         $order->save();
 
         $registration = Registration::find($session->client_reference_id);
-
         $payment_intent = Session::get($session->payment_intent);
         if ($payment_intent == null) {
             if ($registration->reservation) {
@@ -130,6 +171,20 @@ class WebhookController extends Controller
         Session::put($session->payment_intent, $session->payment_intent);
     }
 
+    // Om man har en kod som ger 0 i kostnad
+    private function fullfill_no_cost_order(mixed $session)
+    {
+        $registration = Registration::find($session->client_reference_id);
+            if ($registration->reservation) {
+                // Events to be triggered for an reservation
+                event(new PreRegistrationSuccessEvent($registration));
+            } else {
+                // Events to be triggered for a full registration
+                event(new CompletedRegistrationSuccessEvent($registration));
+            }
+        Session::put($session->payment_intent, $session->payment_intent);
+    }
+
     private function fulfill_final_registration_order($session)
     {
         $order = Order::firstWhere('payment_intent_id', $session->payment_intent);
@@ -139,6 +194,15 @@ class WebhookController extends Controller
         event(new CompletedRegistrationSuccessEvent($registration));
         Session::put($session->payment_intent, $session->payment_intent);
     }
+
+    // Om man har en kod som ger 0 i kostnad
+    private function fulfill_no_cost_final_registration_order(mixed $session)
+    {
+        $registration = Registration::find($session->client_reference_id);
+        event(new CompletedRegistrationSuccessEvent($registration));
+        Session::put($session->payment_intent, $session->payment_intent);
+    }
+
 
     private function failed_order($session)
     {
@@ -166,4 +230,19 @@ class WebhookController extends Controller
         $metadata = $session->metadata;
         event(new FailedPaymentEvent($registration->registration_uid, boolval($metadata->is_final_registration_on_event)));
     }
+
+    private function nocostorder(mixed $session)
+    {
+        if ($session->payment_intent == null) {
+            // kolla om vi har en full_discount
+            if ($session->total_details->amount_discount > 0 && $session->amount_subtotal == $session->total_details->amount_discount) {
+                Log::debug("This is a no cost order for registration " . $session->client_reference_id);
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+
 }
