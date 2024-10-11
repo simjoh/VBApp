de3a89b57f2ebdf1f132226b0b4423f03a947242
Bengt Hellstrom <bengt.hellstrom@umu.se>
2024-10-11 09:17:42 +0200

    mer justeringar i trackingsidor

--- frontend/src/app/core/auth/has-access-to-competitor-functions.guard.ts
+++ frontend/src/app/core/auth/has-access-to-competitor-functions.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HasAccessToCompetitorFunctionsGuard implements CanActivate {
+export class HasAccessToCompetitorFunctionsGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
