6e9f0cfab6d193545a8e71ce7201bd69cacade24
bethem92 <bethem92@gmail.com>
2025-09-17 22:40:56 +0200

    Initial commit for frontend-rider. Split the riders app from admin

---
+++ frontend-rider/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('Rider App');
+}
